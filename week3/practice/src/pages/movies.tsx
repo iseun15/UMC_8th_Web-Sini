ae439743b8fb699c1b5375a7923d85ef3b29546a
iseun15 <ave0315@naver.com>
2025-04-01 17:30:45 +0900

    3주차 워크북 실습 연습습

--- week3/practice/src/pages/movies.tsx
+++ week3/practice/src/pages/movies.tsx
@@ -1,14 +1,37 @@
-import { useParams } from 'react-router-dom';
+import { Movie, MovieResponse } from '../types/movie';
+import { useState, useEffect } from 'react';
 
+import axios from 'axios'; 
 
 const MoviesPage = () => {
-    const params = useParams();
+    const [movies, setMovies ] = useState<Movie[]>([]);
 
-    console.log(params);
+    useEffect(() => {
+        const fetchMovies = async () => {
+            const {data} = await axios.get<MovieResponse>(  //axios : application/json 자동 요청청
+                `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1`,
+                {
+                    headers : {
+                        Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+                    },
+                }
+            );
 
-  return (
-    <h1>{params.movieId}Movies Page </h1>
-  )
-}
+            setMovies(data.results);
+        };
+        fetchMovies();
+    }, []);
+
+    return (
+        <ul>
+            {/*옵셔널 체인 활용*/}
+            {movies?.map((movie)=>(
+                <li key={movie.id}>
+                    <h1>{movie.title}</h1>
+                </li>
+            ))}
+        </ul>
+    );
+};
 
 export default MoviesPage
